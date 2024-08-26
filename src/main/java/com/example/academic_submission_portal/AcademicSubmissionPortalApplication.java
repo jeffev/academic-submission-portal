86379af7bfd3bbb72711fb27efa609d63425bec1
DESKTOP-TP0PGUH\jeffe <jeffev123@gmail.com>
2024-08-26 11:29:21 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/com/example/academic_submission_portal/AcademicSubmissionPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.academic_submission_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSubmissionPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSubmissionPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/academic_submission_portal/AcademicSubmissionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academic_submission_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSubmissionPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
